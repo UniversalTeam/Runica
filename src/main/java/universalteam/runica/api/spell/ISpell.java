41790c4bdfcb843de941ab90cf7d1ffcf1540dbe
big_Xplosion <bigxplosion44@gmail.com>
2014-08-19 17:18:04 +0200

    Added very early basic of a spell, but this system will need some more brainstorming

---
+++ src/main/java/universalteam/runica/api/spell/ISpell.java
@@ -0,0 +1,17 @@
+package universalteam.runica.api.spell;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+public interface ISpell
+{
+	public String getUnlocalizedName();
+
+	public String getDescription();
+
+	public int getPowerUsage();
+
+	public int getPowerFromSurroundings(EntityPlayer player, World world, int x, int y, int z);
+
+	public void castSpell(EntityPlayer player, World world, int x, int y, int z);
+}

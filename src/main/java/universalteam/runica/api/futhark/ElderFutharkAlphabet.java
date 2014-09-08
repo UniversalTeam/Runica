d035e290f0026455aebfbbe1c7781a6470f716bb
SeamusFD <armstrongmediahome@icloud.com>
2014-09-08 15:42:03 -0500

    Changed the names in the ElderFuthark alphabet to match the 24 runes in the alphabet

--- src/main/java/universalteam/runica/api/futhark/ElderFutharkAlphabet.java
+++ src/main/java/universalteam/runica/api/futhark/ElderFutharkAlphabet.java
@@ -10,31 +10,29 @@ public class ElderFutharkAlphabet
 
 	static
 	{
+		alphabet.add("sōwilō");
+		alphabet.add("ūruz");
+		alphabet.add("jēra");
+		alphabet.add("naudiz");
 		alphabet.add("ansuz");
-		alphabet.add("berkanan");
+		alphabet.add("fehu");
+		alphabet.add("þurisaz");
 		alphabet.add("kaunan");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
-		alphabet.add("ansuz");
+		alphabet.add("gebō");
+		alphabet.add("wunjō");
+		alphabet.add("hagalaz");
+		alphabet.add("īsaz");
+		alphabet.add("ēhaz");
+		alphabet.add("perþ-");
+		alphabet.add("algiz");
+		alphabet.add("teiwaz");
+		alphabet.add("berkanan");
+		alphabet.add("mannaz");
+		alphabet.add("laguz");
+		alphabet.add("ingwaz");
+		alphabet.add("ōþala-");
+		alphabet.add("dagaz");
+		alphabet.add("eihwaz");
+		alphabet.add("ehwaz");
 	}
 }
